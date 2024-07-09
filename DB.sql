d0d832ef78cacfca23cb6715e90ba48fa040c85d
qBlackbirdp <blackbird9711@gmail.com>
2024-07-09 16:15:43 +0900

    공부 분석

---
+++ DB.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS `AM_JDBC_2024_07_NOTMINE`;
+CREATE DATABASE `AM_JDBC_2024_07_NOTMINE`;
+USE `AM_JDBC_2024_07_NOTMINE`;
+
+SHOW TABLES;
+
+CREATE TABLE article(
+                        id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                        regDate DATETIME NOT NULL,
+                        updateDate DATETIME NOT NULL,
+                        title CHAR(100) NOT NULL,
+                        `body` TEXT NOT NULL
+);
+
+SELECT *
+FROM article;
+
+SELECT NOW();
+
+SELECT '제목1';
+
+SELECT CONCAT('제목',' 1');
+
+SELECT SUBSTRING(RAND() * 1000 FROM 1 FOR 2);
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
+`body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
+
+SELECT *
+FROM article;
