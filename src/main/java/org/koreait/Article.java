5fe92e88d0ad208369449bd967c0eeaaef55b7c9
qBlackbirdp <blackbird9711@gmail.com>
2024-07-09 17:59:20 +0900

    DBUtil,SecSql 적용중

--- src/main/java/org/koreait/Article.java
+++ src/main/java/org/koreait/Article.java
@@ -1,5 +1,7 @@
 package org.koreait;
 
+import java.util.Map;
+
 public class Article {
     private int id;
     private String regDate;
@@ -13,6 +15,15 @@ public class Article {
         this.updateDate = updateDate;
         this.title = title;
         this.body = body;
+    }
+
+    public Article(Map<String, Object> articleMap) {
+        this.id = (int) articleMap.get("id");
+        this.regDate = (String) articleMap.get("regDate");
+        this.updateDate = (String) articleMap.get("updateDate");
+        this.title = (String) articleMap.get("title");
+        this.body = (String) articleMap.get("body");
+
     }
 
     public String getRegDate() {
